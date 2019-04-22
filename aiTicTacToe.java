8f616ff93b453ce01014831d833e0791cade68c2
Will Munoz <willdmunoz@gmail.com>
2019-04-22 17:16:58 -0400

    added heurstic function

--- aiTicTacToe.java
+++ aiTicTacToe.java
@@ -1,6 +1,24 @@
 import java.util.*;
-public class aiTicTacToe {
 
+import org.w3c.dom.Node;
+public class Tree<T> {
+    private Node<T> root;
+
+    public Tree(T rootData) {
+        root = new Node<T>();
+        root.data = board;
+        root.children = new ArrayList<Node<T>>();
+    }
+
+    public static class Node<T> {
+		public List <positionTicTacToe> boardState;
+		public int heuristicValue;
+        private Node<T> parent;
+        private List<Node<T>> children;
+    }
+}
+
+public class aiTicTacToe {
 	public int player; //1 for player 1 and 2 for player 2
 	private int getStateOfPositionFromBoard(positionTicTacToe position, List<positionTicTacToe> board)
 	{
@@ -8,17 +26,113 @@ public class aiTicTacToe {
 		int index = position.x*16+position.y*4+position.z;
 		return board.get(index).state;
 	}
+
+	//copying the method to make a move on the board
+	public boolean makeMove(positionTicTacToe position, int player, List<positionTicTacToe> targetBoard)
+	{
+		//make move on Tic-Tac-Toe board, given position and player 
+		//player 1 = 1, player 2 = 2
+		
+		//brute force (obviously not a wise way though)
+		for(int i=0;i<targetBoard.size();i++)
+		{
+			if(targetBoard.get(i).x==position.x && targetBoard.get(i).y==position.y && targetBoard.get(i).z==position.z) //if this is the position
+			{
+				if(targetBoard.get(i).state==0)
+				{
+					targetBoard.get(i).state = player;
+					return true;
+				}
+				else
+				{
+					System.out.println("Error: this is not a valid move.");
+				}
+			}
+			
+		}
+		return false;
+	}
+
+	public int getHeuristic(Node node, int MaxPlayer) {
+		List<List<positionTicTacToe>> winningLines = initializeWinningLines();
+		List <positionTicTacToe> boardState = node.boardState;
+		
+		//counting the winning combinations theoretical state of the board
+		for(ArrayList<positionTicTacToe> line: winningLines){
+			int countWins = 0;
+			if (line.contains(position)) {
+				boolean validPlay = true;
+				for(positionTicTacToe winningPosition: line) {
+					//increasing the winPossible count if there's a friendly piece in a winning position
+					if (getStateOfPositionFromBoard(winningPosition,boardState)!=0 && getStateOfPositionFromBoard(winningPosition,boardState)!=playerNum) {
+						validPlay = false;
+					}
+				}
+				if (validPlay) {
+					countWins++;
+				}
+			}
+		}
+		return countWins;
+	}
+
+	public int miniMax(Tree<T> node, int depth, boolean maxPlayer){
+		if(depth == 0 | node.children == 0){
+			return getHeuristic(maxPlayer);
+		}
+		if(maxPlayer == true){
+			value -= -9000;
+			for(i=0; i<node.children.length(); i++){
+				value = max(value, miniMax(children[i]), depth-1, false);
+			return value;
+			}
+			return false;
+		}
+		else {
+			value += 9000;
+			value = -9000;
+			for(i=0; i<node.children.length(); i++){
+				value = max(value, miniMax(children[i]), depth-1, false);
+			}
+			return value;
+		}
+		
+	}
 	public positionTicTacToe myAIAlgorithm(List<positionTicTacToe> board, int player)
 	{
 		//TODO: this is where you are going to implement your AI algorithm to win the game. The default is an AI randomly choose any available move.
 		positionTicTacToe myNextMove = new positionTicTacToe(0,0,0);
-		
+
 		do
 			{
-				Random rand = new Random();
-				int x = rand.nextInt(4);
-				int y = rand.nextInt(4);
-				int z = rand.nextInt(4);
+				//preparing to record all possible moves for the tree
+				Tree<T> rootNode = new Tree<T>(board);
+				rootNode.children = children;
+				List<Node> children = new List<positionTicTacToe>();
+				List<positionTicTacToe> deepCopy = deepCopyATicTacToeBoard(List<positionTicTacToe> board);
+
+				//adding onto the rootNode all possible moves by both players
+				for(positionTicTacToe position: deepCopy){
+					if(getStateOfPositionFromBoard(position, deepCopy) == 0){
+						List<positionTicTacToe> childBoard = deepCopyATicTacToeBoard(List<positionTicTacToe> board);
+						makeMove(position, player, childBoard);
+						Node childNode;
+						childNode.boardState = childBoard;
+						children.add(childNode);
+					}
+
+				}
+
+				//getting the hueristic values of all the minimax
+				List<Integer> scores = new List<Integer>();
+				for(Node winCombo: children){
+					int score = getHeuristic(winCombo, player);
+				}
+				
+
+				//using the minMax to get the score
+				myNextMove = miniMax(board, 2, player);
+
 				myNextMove = new positionTicTacToe(x,y,z);
 			}while(getStateOfPositionFromBoard(myNextMove,board)!=0);
 		return myNextMove;
